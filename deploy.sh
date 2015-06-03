64427515b9eef6a067709b2774e6232eefc9c732
Joey Baker <joey@byjoeybaker.com>
2015-06-03 09:05:47 -0700

    fix start script

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-git pull --rebase
-sudo docker build -t joeybaker/rachelandjoey .
-sudo docker kill rachelandjoey
-sudo docker rm rachelandjoey
-sudo docker run -d  -p 80:8000 --name rachelandjoey joeybaker/rachelandjoey
---
+++ start.sh
@@ -0,0 +1,7 @@
+sudo docker build -t joeybaker/rachelandjoey /srv/rachelandjoey
+sudo docker rm -f rachelandjoey
+sudo docker run -d --name rachelandjoey joeybaker/rachelandjoey
+sudo docker rm -f bud
+sudo docker run -d -v /srv/bud:/data -p 443:443 --name bud --link rachelandjoey:backend joeybaker/bud-tls
+sudo docker rm -f redirector
+sudo docker run -d --restart=always -p 80:80 --name redirector getable/https-redirect

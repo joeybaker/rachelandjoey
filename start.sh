80f4583db5d17490bc7a1bb802c7b0f74ec645ca
Joey Baker <joey@byjoeybaker.com>
2015-06-03 10:16:31 -0700

    add: let’s try rethink

--- start.sh
+++ start.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+# strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
 cd /srv/rachelandjoey.com
 git reset --hard HEAD
 git pull --rebase --prune
 npm prune
 npm i --production
+
+sudo docker run --name rethinkdb -v "/srv/rethinkdb:/data" -d rethinkdb
+
 sudo docker build -t joeybaker/rachelandjoey /srv/rachelandjoey.com
 sudo docker rm -f rachelandjoey
-sudo docker run -d --name rachelandjoey joeybaker/rachelandjoey
+sudo docker run -d --restart=always --name rachelandjoey --link rethinkdb:rdb \
+  joeybaker/rachelandjoey
+
+
 sudo docker rm -f bud
-sudo docker run -d -v /srv/bud:/data -p 443:443 --name bud --link rachelandjoey:backend joeybaker/bud-tls
+sudo docker run -d --restart=always -v "/srv/bud:/data" -p 443:443 --name bud \
+  --link rachelandjoey:backend joeybaker/bud-tls
+
 sudo docker rm -f redirector
-sudo docker run -d --restart=always -p 80:80 --name redirector getable/https-redirect
+sudo docker run -d --restart=always -p 80:80 \
+  --name redirector getable/https-redirect

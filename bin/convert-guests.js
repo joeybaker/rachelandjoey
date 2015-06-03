355b3cb20dbcd09138d0d7f7df1cfd2c53f23360
Joey Baker <joey@byjoeybaker.com>
2015-06-02 18:48:23 -0700

    add: basic convert guests csv script

---
+++ bin/convert-guests.js
@@ -0,0 +1,28 @@
+// {
+//   id: 'partyanme'
+//   , names: [
+//     'name name'
+//     'name name'
+//   ]
+//   , attending: false
+//   , meals: {
+//     name: 'meal'
+//   }
+// }
+
+import _ from 'lodash'
+import fs from 'fs'
+import path from 'path'
+
+const raw = require(path.join(__dirname, '..', 'guests-raw.json'))
+
+const formatted = _(raw)
+  .groupBy('partyName')
+  .map((persons, party) => {
+    const id = 'party:' + _.kebabCase(party)
+    const names = persons.map((person) => `${person.firstName} ${person.lastName}`)
+    return {id, names, attending: null, meals: {}}
+  })
+  .value()
+
+console.log(JSON.stringify(formatted, null, 2))

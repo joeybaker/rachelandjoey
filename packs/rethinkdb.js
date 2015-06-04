5c3c6de7575f4cba55eac0bcec824a07c36fbedc
Joey Baker <joey@byjoeybaker.com>
2015-06-03 18:32:02 -0700

    fix server: remove hardcoded values

--- packs/rethinkdb.js
+++ packs/rethinkdb.js
@@ -1,19 +1,21 @@
 import hapiRethinkdb from '@joeybaker/hapi-rethinkdb'
+import assign from 'lodash/object/assign'
 
 const initRethinkdb = function initRethinkdb (server, callback) {
+  const options = assign({
+    db: 'rachelandjoey'
+    , table: {
+      name: 'rsvp'
+      , indexes: [{
+        name: 'names'
+        , multi: true
+      }]
+    }
+  }, server.app.config.rethinkdb)
+
   server.register({
     register: hapiRethinkdb
-    , options: {
-      // db: 'rachelandjoey'
-      url: 'rethinkdb://192.168.99.100:32768/rachelandjoey'
-      , table: {
-        name: 'rsvp'
-        , indexes: [{
-          name: 'names'
-          , multi: true
-        }]
-      }
-    }
+    , options
   }, callback)
 }
 

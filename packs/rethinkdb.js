764b1efb648b612a3c050fb486bc4d2670b595a3
Joey Baker <joey@byjoeybaker.com>
2015-06-03 18:45:39 -0700

    server: use docker rethinkdb ip

--- packs/rethinkdb.js
+++ packs/rethinkdb.js
@@ -4,6 +4,8 @@ import assign from 'lodash/object/assign'
 const initRethinkdb = function initRethinkdb (server, callback) {
   const options = assign({
     db: 'rachelandjoey'
+    // yea, this is hardcoded for docker
+    , host: process.env.RDB_PORT_28015_TCP_ADDR
     , table: {
       name: 'rsvp'
       , indexes: [{

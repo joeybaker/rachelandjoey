c2606215b0b21ddef2938bd67862241d8d7b8e4b
Joey Baker <joey@byjoeybaker.com>
2015-06-14 11:57:57 -0700

    use react-a11y in dev

--- components/_routes/example/entry.jsx
+++ components/_routes/example/entry.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Routes from '../index.jsx'
 import Router from 'react-router'
 import data from './data.js'
+import a11y from 'react-a11y'
 
 // expose React for debugging
 window.React = React
+a11y(React)
 
 Router.run(Routes, Router.HistoryLocation, (Handler) => {
   React.render(<Handler {...data} />, document.getElementById('app'))

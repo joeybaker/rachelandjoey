2fa1bd92960fe4150f3abb1044a787c47df3b386
Joey Baker <joey@byjoeybaker.com>
2015-06-17 10:48:43 -0700

    only use fast click on the nav
    it seems to mess up the autosuggest?

--- static/entry.js
+++ static/entry.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Routes from '../components/_routes/'
 import Router from 'react-router'
-import FastClick from 'fastclick'
+import fastClick from 'fastclick'
 import data from '../components/_routes/example/data.js'
 
-window.addEventListener('DOMContentLoaded', () => FastClick(document.body), false)
+window.addEventListener('DOMContentLoaded', () => fastClick(document.getElementsByClassName('entry-nav')[0]), false)
 
 Router.run(Routes, Router.HistoryLocation, (Handler) => {
   React.render(<Handler {...data} />, document.getElementById('app'))

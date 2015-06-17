8f1466ac22aeeba79e48f8f371ed6f474f95a9aa
Joey Baker <joey@byjoeybaker.com>
2015-06-17 10:02:47 -0700

    cache html in memory

--- routes/assets-html.js
+++ routes/assets-html.js
@@ -12,24 +12,36 @@ const html = fs.readFileSync(path.join(__dirname, '..', 'static', 'index.html'))
   .replace('/index.js', `/${versions.js}/index.js`)
   .replace('/entry.css', `/${versions.css}/entry.css`)
 
+const htmlCache = {}
+
 const handler = (req, reply) => {
-  ReactRouter.run(ReactRoutes, req.path, (Handler) => {
-    const initialHTML = React.renderToString(React.createElement(Handler, ReactData))
-    let title = 'Rachel & Joey'
-
-    if (React.documentHead) {
-      title = React.documentHead.title
-      React.documentHead = {}
-    }
-
-    const response = reply(html
-      .replace('id="app">', 'id="app">' + initialHTML)
-      .replace('<title>Rachel & Joey</title>', `<title>${title}</title>`)
-    ).type('text/html')
-
+  // TODO: move this to a server method
+  if (htmlCache[req.path]) {
+    const response = reply(htmlCache[req.path]).type('text/html')
     // TODO: remove this. it's because I fucked up and the cache needs to be busted
     response.ttl(expiresIn)
-  })
+  }
+  else {
+    ReactRouter.run(ReactRoutes, req.path, (Handler) => {
+      const initialHTML = React.renderToString(React.createElement(Handler, ReactData))
+      let title = 'Rachel & Joey'
+
+      if (React.documentHead) {
+        title = React.documentHead.title
+        React.documentHead = {}
+      }
+
+      const outHtml = html
+        .replace('id="app">', 'id="app">' + initialHTML)
+        .replace('<title>Rachel & Joey</title>', `<title>${title}</title>`)
+
+      const response = reply(outHtml).type('text/html')
+      htmlCache[req.path] = outHtml
+
+      // TODO: remove this. it's because I fucked up and the cache needs to be busted
+      response.ttl(expiresIn)
+    })
+  }
 }
 
 export default {

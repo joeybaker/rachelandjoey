98a19d0a508b2716f02c971dbd2cf2a9b52f05fb
Joey Baker <joey@byjoeybaker.com>
2015-06-17 10:48:12 -0700

    build script now uses browserify-incremental

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
+const BUILD_TIMER = 'build'
+console.time(BUILD_TIMER)
+
 import md5 from 'MD5'
 import atomifyCSS from 'atomify-css'
 import browserify from 'browserify'
+import browserifyIncremental from 'browserify-incremental'
+import assign from 'lodash/object/assign'
 import path from 'path'
 import fs from 'fs'
 import versions from './versions.json'
@@ -8,19 +13,22 @@ import versions from './versions.json'
 const staticPath = path.join(__dirname, 'static')
 const versionsPath = path.join(__dirname, 'versions.json')
 
-const writeVersions = () => fs.writeFileSync(versionsPath, JSON.stringify(versions, null, 2))
+const writeVersions = () => fs.writeFile(versionsPath, JSON.stringify(versions, null, 2))
 
 const buildJs = function buildJs (callback) {
   console.info(['info', 'http', 'bundle', 'js'], 'started')
-  const b = browserify({
+  const b = browserify(assign({
     debug: true
     , extensions: ['.js', '.json', '.jsx']
-  })
+    , cacheFile: path.join('/tmp', `${path.basename(__dirname)}-browserify-cache.json`)
+  }, browserifyIncremental.args))
 
   b.add(path.join(staticPath, 'entry.js'))
 
   b.transform('babelify')
   b.plugin('minifyify', {map: '/static/index.js.map'})
+
+  browserifyIncremental(b)
 
   b.bundle((...args) => {
     console.info(['info', 'http', 'bundle', 'js'], 'finished')
@@ -31,8 +39,8 @@ buildJs(function bundled (err, js, map) {
   if (err) throw err
 
   versions.js = md5(js)
-  fs.writeFileSync(path.join(staticPath, 'index.js'), js)
-  fs.writeFileSync(path.join(staticPath, 'index.js.map'), map)
+  fs.writeFile(path.join(staticPath, 'index.js'), js)
+  fs.writeFile(path.join(staticPath, 'index.js.map'), map)
   writeVersions()
 })
 
@@ -50,6 +58,8 @@ const buildCss = function buildCss (callback) {
 buildCss(function builtCss (err, css) {
   if (err) throw err
   versions.css = md5(css)
-  fs.writeFileSync(path.join(staticPath, 'entry.css'), css)
+  fs.writeFile(path.join(staticPath, 'entry.css'), css)
   writeVersions()
 })
+
+process.on('exit', () => console.timeEnd(BUILD_TIMER))

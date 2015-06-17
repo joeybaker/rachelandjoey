4f0bcffa1bdca8dec2227bb923a0ebcc52a6f28e
Joey Baker <joey@byjoeybaker.com>
2015-06-17 10:48:24 -0700

    es6-ify the main server file

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 process.title = 'rachelandjoey'
 
-
 import hapi from 'hapi'
-const path = require('path')
-  , jace = require('jace')
-  , config = jace({
-    configPath: path.join(__dirname, 'config')
-  })
-  , bulkRequire = require('bulk-require')
-  , server = new hapi.Server()
-  , packs = bulkRequire(__dirname, ['packs/*.js']).packs
-  , routes = bulkRequire(__dirname, ['routes/*.js']).routes
-  , _ = require('lodash')
+import path from 'path'
+import jace from 'jace'
+import bulkRequire from 'bulk-require'
+import each from 'lodash/collection/each'
+import values from 'lodash/object/values'
+import after from 'lodash/function/after'
+import size from 'lodash/collection/size'
 
+const packs = bulkRequire(__dirname, ['packs/*.js']).packs
+const routes = bulkRequire(__dirname, ['routes/*.js']).routes
+const config = jace({
+  configPath: path.join(__dirname, 'config')
+})
+
+// setup server
+const server = new hapi.Server()
 server.app.config = config
 
 const connectionOptions = {port: 8000}
 if (config.nodeEnv === 'development') connectionOptions.host = 'localhost'
 server.connection(connectionOptions)
 
+// add routes
+server.route(values(routes))
 
-server.route(_.values(routes))
-
-const startServer = _.after(_.size(packs), function startServer () {
-  server.start(function serverStarted () {
-    server.log(['server', 'init'], server.info)
-  })
+// load in packs and start server
+const startServer = after(size(packs), () => {
+  server.start(() => server.log(['server', 'init'], server.info))
 })
-_.each(packs, function startPack (pack) {
-  pack(server, startServer)
-})
+each(packs, (pack) => pack(server, startServer))

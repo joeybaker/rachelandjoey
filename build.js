6ebd292a77c69d3ee2f31b465979e231a8629b01
Joey Baker <joey@byjoeybaker.com>
2015-06-29 15:53:01 -0700

    add build for dashboard assets

--- build.js
+++ build.js
@@ -12,30 +12,33 @@ import versions from './versions.json'
 
 const staticPath = path.join(__dirname, 'static')
 const versionsPath = path.join(__dirname, 'versions.json')
+const componentPath = path.join(__dirname, 'components')
 
 const writeVersions = () => fs.writeFile(versionsPath, JSON.stringify(versions, null, 2))
 
-const buildJs = function buildJs (callback) {
-  console.info(['info', 'http', 'bundle', 'js'], 'started')
+const buildJs = function buildJs (inPath, mapPath, callback) {
+  const entryName = path.basename(mapPath)
+  console.info(['info', entryName, 'bundle', 'js'], 'started')
   const b = browserify(assign({
     debug: true
     , extensions: ['.js', '.json', '.jsx']
-    , cacheFile: path.join('/tmp', `${path.basename(__dirname)}-browserify-cache.json`)
+    , cacheFile: path.join('/tmp', `${path.basename(__dirname)}-${entryName}-browserify-cache.json`)
   }, browserifyIncremental.args))
 
-  b.add(path.join(staticPath, 'entry.js'))
+  b.add(inPath)
 
   b.transform('babelify')
-  b.plugin('minifyify', {map: '/static/index.js.map'})
+  b.plugin('minifyify', {map: mapPath})
 
   browserifyIncremental(b)
 
   b.bundle((...args) => {
-    console.info(['info', 'http', 'bundle', 'js'], 'finished')
+    console.info(['info', entryName, 'bundle', 'js'], 'finished')
     callback.apply(null, args)
   })
 }
-buildJs(function bundled (err, js, map) {
+
+buildJs(path.join(staticPath, 'entry.js'), '/static/index.js.map', function bundled (err, js, map) {
   if (err) throw err
 
   versions.js = md5(js)
@@ -44,22 +47,42 @@ buildJs(function bundled (err, js, map) {
   writeVersions()
 })
 
-const buildCss = function buildCss (callback) {
-  console.info(['info', 'http', 'bundle', 'css'], 'started')
+buildJs(path.join(componentPath, 'page-dashboard', 'example', 'entry.jsx'), '/static/dashboard.js.map', function bundled (err, js, map) {
+  if (err) throw err
+
+  versions.jsDashboard = md5(js)
+  fs.writeFile(path.join(staticPath, 'dashboard.js'), js)
+  fs.writeFile(path.join(staticPath, 'dashboard.js.map'), map)
+  writeVersions()
+})
+
+
+const buildCss = function buildCss (entry, callback) {
+  const entryName = path.basename(entry)
+  console.info(['info', entryName, 'bundle', 'css'], 'started')
   atomifyCSS({
-    entry: path.join(__dirname, 'components', '_routes', 'index.css')
+    entry
     , compress: true
     , autoprefixer: true
   }, (...args) => {
-    console.info(['info', 'http', 'bundle', 'css'], 'finished')
+    console.info(['info', entryName, 'bundle', 'css'], 'finished')
     callback.apply(null, args)
   })
 }
-buildCss(function builtCss (err, css) {
+
+buildCss(path.join(componentPath, '_routes', 'index.css'), function builtCss (err, css) {
   if (err) throw err
   versions.css = md5(css)
   fs.writeFile(path.join(staticPath, 'entry.css'), css)
   writeVersions()
 })
+
+buildCss(path.join(componentPath, 'page-dashboard', 'index.css'), function builtCss (err, css) {
+  if (err) throw err
+  versions.cssDashboard = md5(css)
+  fs.writeFile(path.join(staticPath, 'dashboard.css'), css)
+  writeVersions()
+})
+
 
 process.on('exit', () => console.timeEnd(BUILD_TIMER))
